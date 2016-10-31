64e50d4049a6d63ce880bfdf86685223b59e9d5c
Trevor <trevdrury@gmail.com>
2016-10-31 20:50:17 +0000

    Responds with parsed values from header object

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
-    res.send("Hello World!");
+    var headers = req.headers;
+    var ip = headers['x-forwarded-for'];
+    var lang = headers['accept-language'].substr(0, headers['accept-language'].indexOf(','));
+    var software = headers['user-agent'].match(/\(([^(]+)\)/);
+    res.json({
+        "ipaddress": ip, "language": lang, "software": software[1]
+    });
 });
 
-app.listen(8080, function(){
+app.listen(port, function(){
     console.log("radical!");
 });
